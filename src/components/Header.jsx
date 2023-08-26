46b3e5b50297e273b25b54e4c01d32e2d9ebd78e
Mateo <mateobetancurb@gmail.com>
2023-08-26 13:24:24 -0500

    relative paths - header component icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,12 @@ const Header = () => {
 			<header className="header">
 				<nav className="header__nav">
 					<div className="header__container">
-						<img src="/src/assets/icons/menu.svg" alt="Menu" />
-						<img src="/src/assets/icons/symbol.svg" alt="Logo" />
+						<img src="/icons/menu.svg" alt="Menu" />
+						<img src="/icons/symbol.svg" alt="Logo" />
 					</div>
 					<div className="header__container shopping__cart">
-						<img src="/src/assets/icons/search.svg" alt="Search icon" />
-						<img src="/src/assets/icons/cart.svg" alt="Cart icon" />
+						<img src="/icons/search.svg" alt="Search icon" />
+						<img src="/icons/cart.svg" alt="Cart icon" />
 						<div className="header__shopping--circle">
 							<span>9</span>
 						</div>
