1f5b3a0f4d28662218e82eaaebd0570d428684fe
Mateo <mateobetancurb@gmail.com>
2023-08-26 12:38:36 -0500

    rendering app components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Header } from "./components/Header";
+import { ProductList } from "./components/ProductList";
+import "./index.scss";
+
 const App = () => {
 	return (
 		<>
-			<h1>Hola mundo</h1>
+			<Header />
+			<ProductList />
 		</>
 	);
 };
