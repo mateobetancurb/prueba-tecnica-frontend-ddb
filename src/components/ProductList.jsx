5f2d9f0ba036688d48d53a547cecf1f187028d56
Mateo <mateobetancurb@gmail.com>
2023-08-26 14:17:42 -0500

    product list app component

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -18,41 +18,43 @@ const ProductList = () => {
 
 	return (
 		<>
-			<h1 className="product__general--title">Cervezas</h1>
-			<section className="product__list--container">
-				{loading ? (
-					<p>Loading...</p>
-				) : (
-					productList.map((product) => (
-						<div key={product?.id} className="product__container">
-							<div className="product__info">
-								<img
-									src={product?.image}
-									alt={product?.name}
-									className="product__image"
-								/>
-								<h3 className="product__name">{product?.name}</h3>
-								<p className="product__description">{product?.description}</p>
-								<p className="product__price">${product?.price}</p>
+			<main className="main">
+				<h1 className="product__general--title">Cervezas</h1>
+				<section className="product__list--container">
+					{loading ? (
+						<p>Loading...</p>
+					) : (
+						productList.map((product) => (
+							<div key={product?.id} className="product__container">
+								<div className="product__info">
+									<img
+										src={product?.image}
+										alt={product?.name}
+										className="product__image"
+									/>
+									<h3 className="product__name">{product?.name}</h3>
+									<p className="product__description">{product?.description}</p>
+									<p className="product__price">${product?.price}</p>
+								</div>
+								<div className="product__button-container">
+									<p className="product__button">AGREGAR</p>
+								</div>
 							</div>
-							<div className="product__button-container">
-								<p className="product__button">AGREGAR</p>
-							</div>
-						</div>
-					))
-				)}
-			</section>
-			<section className="section__continue--shopping">
+						))
+					)}
+				</section>
 				<div className="continue__shopping--container">
-					<p className="continue__shopping--p">Sigue mercando</p>
-					<h3 className="continue__shopping--h3">Vinos</h3>
+					<div>
+						<p className="continue__shopping--p">Sigue mercando</p>
+						<h3 className="continue__shopping--h3">Vinos</h3>
+					</div>
+					<img
+						src="/icons/arrow.svg"
+						alt="Arrow right"
+						className="continue__shopping--icon"
+					/>
 				</div>
-				<img
-					src="/src/assets/icons/arrow.svg"
-					alt="Arrow right"
-					className="continue__shopping--icon"
-				/>
-			</section>
+			</main>
 		</>
 	);
 };
