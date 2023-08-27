84edd94e86211f8cd5d851a4c2c4cdb023b887ea
Mateo <mateobetancurb@gmail.com>
2023-08-27 18:58:06 -0500

    custom hook - global state

---
+++ src/hooks/useGlobalState.jsx
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+
+import { AppContext } from "../context/AppProvider";
+
+const useGlobalState = () => {
+	return useContext(AppContext);
+};
+
+export { useGlobalState };
