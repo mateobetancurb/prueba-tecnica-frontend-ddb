a2146633282f49de8a2ad0c733ee2ac0483e801e
Mateo <mateobetancurb@gmail.com>
2023-08-26 23:06:59 -0500

    props to children components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import { Header } from "./components/Header";
 import { ProductList } from "./components/ProductList";
 import { Footer } from "./components/Footer";
 import "./index.scss";
 
 const App = () => {
+	const [selectedProductId, setSelectedProductId] = useState(null);
 	return (
 		<>
 			<Header />
-			<ProductList />
-			<Footer />
+			<ProductList selectedProductId={selectedProductId} />
+			<Footer setSelectedProductId={setSelectedProductId} />
 		</>
 	);
 };
