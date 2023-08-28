e2ac3808fe0ebf16c5632a60f9412ca51a94d36f
Mateo <mateobetancurb@gmail.com>
2023-08-27 19:03:34 -0500

    config - global state

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { AppProvider } from "./context/AppProvider.jsx";
 import { App } from "./App.jsx";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<AppProvider>
+			<App />
+		</AppProvider>
 	</React.StrictMode>
 );
